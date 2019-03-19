6f96abf35c3c8a0e58ded5b10ca34431aeae7103
Trix Farrar <trix@basement.net>
2019-03-19 15:02:37 -0500

    Fix manual path

--- packaging/common/freebsd/profile.sh
+++ packaging/common/freebsd/profile.sh
@@ -2,6 +2,6 @@ if ! echo ${PATH} | /bin/grep @@PREFIX@@/bin > /dev/null ; then
     export PATH=$PATH:@@PREFIX@@/bin
 fi
 
-if ! echo ${MANPATH} | /bin/grep @@PREFIX@@/share/man > /dev/null ; then
-    export MANPATH=$MANPATH:@@PREFIX@@/share/man
+if ! echo ${MANPATH} | /bin/grep @@PREFIX@@/man > /dev/null ; then
+    export MANPATH=$MANPATH:@@PREFIX@@/man
 fi
